d6b654598a52dd63035627266f49ca6b351b9270
Erik Andreazi <erik@treetools.com.br>
2022-11-24 21:51:53 -0300

    Testando a criacao e associacao dos SG

---
+++ modulo-03-ec2/vpc_sg_tcb_terraform.tf
@@ -0,0 +1,27 @@
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
+#   vpc_id      = aws_vpc.main.id
+
+#   ingress {
+#     description      = "SSH from VPC"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = [aws_vpc.main.cidr_block]
+#   }
+# }
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+}
